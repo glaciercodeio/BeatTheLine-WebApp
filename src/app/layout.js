4a4822b18fc1795f95ff461055eb66adfed70f1f
Fabian De la cruz <fabiandelacruzdc@gmail.com>
2025-01-07 21:34:40 -0700

    Add new global font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { montserrat } from '@/app/components/ui/fonts';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${montserrat.className} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
