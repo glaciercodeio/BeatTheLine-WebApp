a358d8372df4d5b85269c356031516827f673b5b
Luis <liestradaz@gmail.com>
2025-01-17 01:26:58 -0700

    Fixed bug in theme color

--- src/app/components/ui/Navbar.jsx
+++ src/app/components/ui/Navbar.jsx
@@ -1,75 +1,89 @@
-'use client';
+"use client";
 import { useState, useEffect } from "react";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 
 export default function Navbar({ initialBackground = false }) {
-    const [isScrolled, setIsScrolled] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+  const pathname = usePathname();
 
-    useEffect(() => {
-        const handleScroll = () => {
-            if (window.scrollY > 0) {
-                setIsScrolled(true);
-            } else {
-                setIsScrolled(false);
-            }
-        };
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
 
-        window.addEventListener("scroll", handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-    const backgroundClass = initialBackground || isScrolled ? "bg-black" : "bg-transparent";
+  const isHomePage = pathname === "/";
+  const backgroundClass =
+    isHomePage && !isScrolled ? "bg-transparent" : "bg-black";
+  //const backgroundClass =
+  //initialBackground || isScrolled ? "bg-black" : "bg-transparent";
 
-    return (
-        <nav
-            className={`flex justify-between items-center px-8 py-4 sm:px-20 fixed top-0 left-0 w-full z-30 transition-colors duration-300 ${backgroundClass}`}
-        >
-            <div className="flex items-center gap-4">
-                <Link href="/">
-                    <Image
-                        src="/BTL-Logo-Horz-Lt-Solid.png"
-                        alt="Website Logo"
-                        width={130}
-                        height={50}
-                    />
-                </Link>
-            </div>
+  //const backgroundClass =
+  //  initialBackground || isScrolled
+  //     ? "bg-black"
+  //    : "bg-black bg-opacity-90 dark:bg-transparent shadow-md";
 
-            <ul className="hidden sm:flex gap-20 text-lg font-medium">
-                <li>
-                    <Link href="/" className="hover:text-[#72D53C] text-white">
-                        Home
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/subscriptions" className="hover:text-[#72D53C] text-white">
-                        Subscriptions
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/aboutUs" className="hover:text-[#72D53C] text-white">
-                        About
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/" className="hover:text-[#72D53C] text-white">
-                        Contact
-                    </Link>
-                </li>
-            </ul>
+  return (
+    <nav
+      className={`flex justify-between items-center px-8 py-4 sm:px-20 fixed top-0 left-0 w-full z-30 transition-colors duration-300 ${backgroundClass}`}
+    >
+      <div className="flex items-center gap-4">
+        <Link href="/">
+          <Image
+            src="/BTL-Logo-Horz-Lt-Solid.png"
+            alt="Website Logo"
+            width={130}
+            height={50}
+          />
+        </Link>
+      </div>
 
-            <div className="flex items-center gap-4">
-                <button className="px-4 py-2 text-sm font-medium text-white border border-[#333333] rounded hover:bg-[#131313]">
-                    Log in
-                </button>
-                <button className="px-4 py-2 text-sm font-medium text-black bg-white rounded hover:bg-gray-100">
-                    Sign up
-                </button>
-            </div>
-        </nav>
-    );
+      <ul className="hidden sm:flex gap-20 text-lg font-medium">
+        <li>
+          <Link href="/" className="hover:text-[#72D53C] text-white">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/subscriptions"
+            className="hover:text-[#72D53C] text-white"
+          >
+            Subscriptions
+          </Link>
+        </li>
+        <li>
+          <Link href="/aboutUs" className="hover:text-[#72D53C] text-white">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link href="/" className="hover:text-[#72D53C] text-white">
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+      <div className="flex items-center gap-4">
+        <button className="px-4 py-2 text-sm font-medium text-white border border-[#333333] rounded hover:bg-[#131313]">
+          Log in
+        </button>
+        <button className="px-4 py-2 text-sm font-medium text-black bg-white rounded hover:bg-gray-100">
+          Sign up
+        </button>
+      </div>
+    </nav>
+  );
 }
--- src/app/subscriptions/page.jsx
+++ src/app/subscriptions/page.jsx
@@ -1,116 +1,133 @@
 import React from "react";
-import Navbar from '@/app/components/ui/Navbar';
+import Navbar from "@/app/components/ui/Navbar";
 
 const plans = [
-    {
-        title: "NFL WEEK PACKAGE ",
-        price: "34.99",
-        description: "> 20-30 plays a week",
-        features: ["Mainly player props", "Include derivatives main markets (lower limit bets)"],
-    },
-    {
-        title: "NCAA Men’s Basketball WEEK PACKAGE ",
-        price: "34.99",
-        description: "> 20-30 plays a week",
-        features: ["Mainly player props", "One main market a day + derivatives main markets"],
-    },
-    {
-        title: "NCAA Men’s Football WEEK PACKAGE ",
-        price: "24.99",
-        description: "> 10-20 plays a week",
-        note: "(VARIES BASED ON SLATE)",
-        features: ["Mainly player props", "Include derivatives main markets"],
-    },
-    {
-        title: "NBA WEEK PACKAGE",
-        price: "69.99",
-        description: "> 50-75 plays a week",
-        features: ["Mainly player props", "Include derivatives main markets"],
-    },
-    {
-        title: "NHL WEEK PACKAGE",
-        price: "29.99",
-        description: "> 15-25 plays a week",
-        features: ["Mainly player props", "Include derivatives main markets"],
-    },
-    {
-        title: "WNBA WEEK PACKAGE",
-        price: "69.99",
-        description: "> 50-75 plays a week",
-        note: "(VARIES BASED ON SLATE)",
-        features: ["Mainly player props", "Include derivatives main markets"],
-    },
-    {
-        title: "MLB WEEK PACKAGE",
-        price: "34.99",
-        description: "> 30-40 plays a week",
-        features: ["Mainly player props", "Include derivatives main markets"],
-    }/*,
+  {
+    title: "NFL WEEK PACKAGE ",
+    price: "34.99",
+    description: "> 20-30 plays a week",
+    features: [
+      "Mainly player props",
+      "Include derivatives main markets (lower limit bets)",
+    ],
+  },
+  {
+    title: "NCAA Men’s Basketball WEEK PACKAGE ",
+    price: "34.99",
+    description: "> 20-30 plays a week",
+    features: [
+      "Mainly player props",
+      "One main market a day + derivatives main markets",
+    ],
+  },
+  {
+    title: "NCAA Men’s Football WEEK PACKAGE ",
+    price: "24.99",
+    description: "> 10-20 plays a week",
+    note: "(VARIES BASED ON SLATE)",
+    features: ["Mainly player props", "Include derivatives main markets"],
+  },
+  {
+    title: "NBA WEEK PACKAGE",
+    price: "69.99",
+    description: "> 50-75 plays a week",
+    features: ["Mainly player props", "Include derivatives main markets"],
+  },
+  {
+    title: "NHL WEEK PACKAGE",
+    price: "29.99",
+    description: "> 15-25 plays a week",
+    features: ["Mainly player props", "Include derivatives main markets"],
+  },
+  {
+    title: "WNBA WEEK PACKAGE",
+    price: "69.99",
+    description: "> 50-75 plays a week",
+    note: "(VARIES BASED ON SLATE)",
+    features: ["Mainly player props", "Include derivatives main markets"],
+  },
+  {
+    title: "MLB WEEK PACKAGE",
+    price: "34.99",
+    description: "> 30-40 plays a week",
+    features: ["Mainly player props", "Include derivatives main markets"],
+  } /*,
     {
         title: "SEASON PACKAGE",
         price: "TBD",
         description: "o	Create when we have the training course created",
         note: "(VARIES BASED ON SLATE)",
         features: ["Mainly player props", "Include derivatives main markets"],
-    },*/
+    },*/,
 ];
 
 export default function Subscriptions() {
-    return (
-        <>
-          <Navbar initialBackground={false} />
-       
-        <div
-            className="min-h-screen flex flex-col bg-white relative "
-            style={{
-                background: `radial-gradient(circle at top, rgba(114, 213, 60, 0.12) 0%, rgba(255, 255, 255, 0) 60%)`,
-            }}>
+  return (
+    <>
+      <Navbar initialBackground={false} />
 
-            <h3 className="text-4xl font-bold text-center mt-32 text-ellipsis text-white">
-                Choose your plan
-            </h3>
+      <div
+        className="min-h-screen flex flex-col bg-white relative "
+        style={{
+          background: `radial-gradient(circle at top, rgba(114, 213, 60, 0.12) 0%, rgba(255, 255, 255, 0) 60%)`,
+        }}
+      >
+        <h3 className="text-4xl font-bold text-center mt-32 text-ellipsis">
+          Choose your plan
+        </h3>
 
-            <div className="flex-grow flex flex-wrap justify-center gap-6 p-6 w-full max-w-6xl mx-auto mt-10 items-start">
-                {plans.map((plan, index) => (
-                    <div
-                        key={index}
-                        className="w-full sm:w-80 rounded-xl p-6 border border-[#252525] hover:border-green-700 transition duration-300 flex flex-col"
-                        style={{ minHeight: "550px", maxHeight: "550px", overflow: "hidden" }}>
-                        <div className="pb-4">
-                            <h3
-                                className="text-lg font-semibold text-lefttext-white">
-                                {plan.title}
-                            </h3>
-                        </div>
+        <div className="flex-grow flex flex-wrap justify-center gap-6 p-6 w-full max-w-6xl mx-auto mt-10 items-start">
+          {plans.map((plan, index) => (
+            <div
+              key={index}
+              className="w-full sm:w-80 rounded-xl p-6 border border-[#252525] hover:border-green-700 hover:bg-green-50 dark:hover:bg-gray-800 hover:shadow-lg transition duration-300 flex flex-col"
+              style={{
+                minHeight: "550px",
+                maxHeight: "550px",
+                overflow: "hidden",
+              }}
+            >
+              <div className="pb-4">
+                <h3 className="text-lg font-semibold text-left">
+                  {plan.title}
+                </h3>
+              </div>
 
-                        <div className="mb-4">
-                            <div className="flex items-end gap-2">
-                                <p className="text-5xl font-medium text-left text-white">
-                                    <span className="text-2xl font-medium text-[#9e9e9e]">$</span>
-                                    {plan.price}
-                                </p>
+              <div className="mb-4">
+                <div className="flex items-end gap-2">
+                  <p className="text-5xl font-medium text-left">
+                    <span className="text-2xl font-medium text-[#9e9e9e]">
+                      $
+                    </span>
+                    {plan.price}
+                  </p>
 
-                                <p className="text-sm font-medium text-[#9e9e9e]">{plan.description}</p>
-                            </div>
+                  <p className="text-sm font-medium text-[#9e9e9e]">
+                    {plan.description}
+                  </p>
+                </div>
 
-                            <p className="text-sm mt-2 font-medium text-[#9e9e9e]">{plan.note}</p>
-                        </div>
+                <p className="text-sm mt-2 font-medium text-[#9e9e9e]">
+                  {plan.note}
+                </p>
+              </div>
 
-                        <ul className="mb-6 space-y-2 text-sm text-white overflow-hidden">
-                            {plan.features.map((feature, i) => (
-                                <li key={i} className="flex items-center gap-2">
-                                    <span className="text-green-700 font-bold">✔</span> {feature}
-                                </li>
-                            ))}
-                        </ul>
-
-                        <button className="w-full py-3 px-4 rounded-md font-semibold transition bg-white text-black hover:bg-green-700 hover:text-white mt-auto">
-                            Select Plan
-                        </button>
-                    </div>
+              <ul className="mb-6 space-y-2 text-sm overflow-hidden">
+                {plan.features.map((feature, i) => (
+                  <li key={i} className="flex items-center gap-2">
+                    <span className="text-green-700 font-bold">✔</span>{" "}
+                    {feature}
+                  </li>
                 ))}
+              </ul>
+
+              <button className="w-full py-3 px-4 rounded-md font-semibold transition bg-white text-black hover:bg-green-700 hover mt-auto">
+                Select Plan
+              </button>
             </div>
-        </div >
-        </>
-    );
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
